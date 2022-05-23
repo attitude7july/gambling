231bfba29068f4694e2dde2f1484c86114479307
Shahid Kochak <attitude_7july@hotmail.com>
2022-05-23 08:25:07 +0200

    tests

---
+++ tests/Application.UnitTests/Common/Mappings/MappingTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Runtime.Serialization;
+using AutoMapper;
+using gambling.Application.Account.Models;
+using gambling.Application.Common.Mappings;
+using gambling.Domain.Entities;
+using NUnit.Framework;
+
+namespace gambling.Application.UnitTests.Common.Mappings;
+
+public class MappingTests
+{
+    private readonly IConfigurationProvider _configuration;
+    private readonly IMapper _mapper;
+
+    public MappingTests()
+    {
+        _configuration = new MapperConfiguration(config => 
+            config.AddProfile<MappingProfile>());
+
+        _mapper = _configuration.CreateMapper();
+    }
+
+    [Test]
+    public void ShouldHaveValidConfiguration()
+    {
+        _configuration.AssertConfigurationIsValid();
+    }
+
+    [Test]
+    [TestCase(typeof(ApplicationUser), typeof(ApplicationUserDto))]
+    [TestCase(typeof(ApplicationUserBet), typeof(ApplicationUserDto))]
+    public void ShouldSupportMappingFromSourceToDestination(Type source, Type destination)
+    {
+        var instance = GetInstanceOf(source);
+
+        _mapper.Map(instance, source, destination);
+    }
+
+    private object GetInstanceOf(Type type)
+    {
+        if (type.GetConstructor(Type.EmptyTypes) != null)
+            return Activator.CreateInstance(type)!;
+
+        // Type without parameterless constructor
+        return FormatterServices.GetUninitializedObject(type);
+    }
+}
